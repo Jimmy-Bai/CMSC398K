53640e27a427f53b0591970aa619266af8e13a74
Jimmy-Bai <jbai0616@gmail.com>
2020-03-28 05:05:12 -0400

    Added Heroku ports

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/',function(req,res){
   res.render('home',{});
 })
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+// Added Heroku ports
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening!');
 });
